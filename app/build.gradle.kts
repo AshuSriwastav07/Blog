e68232de4eba259c8c92d92397a8395a7166b0ea
Ashuriwastav07 <ashusriwastav58@gmail.com>
2024-08-23 13:55:05 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.gms:play-services-auth:21.2.0")
-    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation(libs.play.services.auth)
+    implementation (libs.picasso)
 
 }
